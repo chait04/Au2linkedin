92c9766942a542a5bbecdfe9a9924d1118077271
Chaitanya Khachane <ckhachane67@gmail.com>
2021-05-27 14:37:14 +0530

    see all button updated and delay added

--- linkedin.js
+++ linkedin.js
@@ -7,57 +7,79 @@ const app = async() => {
         ignoreDefaultArgs: ["--enable-automation"]
     })
     const page = await browser.newPage()
+
+    // This line makes the newPage full screen
     await page._client.send("Emulation.clearDeviceMetricsOverride")
+
+    // Navigating to linkedin
     await page.goto(`https://www.linkedin.com/`)
     
 
-// =================login --username & password=============================
-    
+// ================= login username & password =============================  
     await page.waitForSelector(`#session_key`)
-    // put your username here
-    
-    await page.type(`#session_key`, `iDontKnow`)
-    // put your password here
 
-    await page.type(`#session_password`, `weDontDoThatHere`)
+    // put your username-                      :here
+    await page.type(`#session_key`,      `Your_email_here`)
+    
+    // put your password-                      :here
+    await page.type(`#session_password`, `Your_Password_here`)
+   
+
+
     // clicking on signon button
     await page.click(`.sign-in-form__submit-button`)
     console.log(`login successful`);
 
-// waiting for the myNetwork to load
+
+
+    // calling waitForTimeout to wait for everything to load on web page
+    console.log('before waiting');
+    await page.waitForTimeout(10000)
+    console.log('after waiting');
+
+
+   // waiting for the myNetwork to load
     await page.waitForXPath(`//*[@id="ember21"]`)
     const [networkButton] = await page.$x(`//*[@id="ember21"]`)
     networkButton.click()
-    console.log(`neteork tab opned`);
+    console.log(`network tab opened`);
+
 
 
 //======================= closing chatting tab================================
-// ye chatting tab band nhi ho rha ...iska class ka issue hai
-    await page.waitForSelector(`.msg-overlay-bubble-header__details.flex-row align-items-center.ml1`)
-    await page.click(`.msg-overlay-bubble-header__details flex-row.align-items-center.ml1`)
-    console.log(`chatting tab closed`);
+//                                     ---Your Linkedin Name here---
+    await page.waitForSelector(`img[title="Chaitanya Khachane"]`)
+    try {
+        await page.click(`img[title="Chaitanya Khachane"]`)        
+    } finally {
+        console.log(`chatting tab closed`);
+    }
+
 
 
 // ======================see-all(button)==============================================
-    // ye see-all ko band rakh ke bhi req accept ho rhi hai..so jyada req aane ke baad check karenge
 
-// wating for see-all button to load
-    // await page.waitForXPath(`/html/body/div[6]/div[3]/div/div/div/div/div/div/div/main/section/header/a`)
-    // const [seeAllButton] = await page.$x(`/html/body/div[6]/div[3]/div/div/div/div/div/div/div/main/section/header/a`)
-    // seeAllButton.click()
-
+    // wating for see-all button to load
+    await page.waitForSelector(`a[data-control-name="manage_all_invites"]`);
+    await page.click(`a[data-control-name="manage_all_invites"]`)
+    console.log(`see all got clicked`);
     
 
-//============================== Main functionality=========================================
-    await page.evaluate(() => {
-        const req = document.getElementsByClassName(`invitation-card__action-btn artdeco-button artdeco-button--2 artdeco-button--secondary ember-view`)
-        let i = 0
-        while(req.length > 1){
-            req[i].click();
-            i++
-        }
-    })
 
+//============================== Main functionality=========================================
+    // The evaluate function runs the below code in that browser
+    try {
+        await page.evaluate(() => {
+            const req = document.getElementsByClassName(`invitation-card__action-btn artdeco-button artdeco-button--2 artdeco-button--secondary ember-view`)
+            let i = 0
+            while(req.length > 1){
+                req[i].click();
+                i++
+            }
+        })    
+    } finally {
+        console.log(`All Request Accepted`);
+    }
 }
 
 app()
